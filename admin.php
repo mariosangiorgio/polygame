d6c211b6a6a01ab405406cb4e6e5dd829962174d
gtrive.code <gtrive.code@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-09-06 23:30:23 +0000

    New files

--- admin.php
+++ admin.php
@@ -1,5 +1,6 @@
 Here the admin should do some cool stuffs!!!<BR>
 <A HREF=newOrganizer.php>Add a new organizer</A><BR>
+<A HREF=newWedge.php>Add a new wedge</A><BR><BR>
 <A HREF=showWedges.php>Veiw the available wedges</A><BR>
-<A HREF=newWedge.php>Add a new wedge</A><BR>
+<A HREF=newWedge.php>Add a new wedge</A><BR><BR>
 <A HREF=logout.php>Logout</A><BR>
