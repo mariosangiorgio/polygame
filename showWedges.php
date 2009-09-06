93eae2d9d2f87a86db29c3778caeba5e324d7b42
mariosangiorgio <mariosangiorgio@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-09-06 23:23:55 +0000

    Added the page to see the list of wedges

---
+++ showWedges.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require("./database/databaseLogin.php");
+
+if( $_SESSION['loggedIn'] == "yes" and
+	$_SESSION['role'] == "administrator"){
+	
+	$query		= "SELECT `Title` FROM `Wedges`;";
+	$data		= mysql_query($query,$connection);
+	
+	while( $row	= mysql_fetch_array($data)){
+		print "A HREF=editWedge.php?wedge=".$row['Title'].">".$row['Title']."</A><BR>";
+	}
+	
+}
+else {
+	print "You must log in as an administrator to access this page!";
+}
+?>
