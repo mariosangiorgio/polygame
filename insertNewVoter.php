29d77546e0cb36ec6f4c3538d142c05b3a408f30
gtrive.code <gtrive.code@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-09-06 23:46:10 +0000

    New files

---
+++ insertNewVoter.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+//Security check
+if( $_SESSION['loggedIn'] == "yes" and
+	$_SESSION['role'] == "organizer"){
+	require("./database/databaseLogin.php");
+	//Sanitizing inputs
+	$username	= mysql_real_escape_string($_POST['username']);
+	$password	= mysql_real_escape_string($_POST['password']);
+	//Hashing and salting the password
+	$password	= sha1("polygame".$password);
+	//Query
+	$query		= "INSERT INTO `Users` (`username`,`role`,`password`)
+				   VALUES ('$username', 'voter', '$password');";
+	$data		= mysql_query($query,$connection);
+	//Redirect to the main page
+	header("Location: index.php");
+}
+else{
+	print "To perform this operation you must be logged in as an organizer!";
+}
+?>
