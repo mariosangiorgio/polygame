f8561ff19090eb1eb86254eaba93d67dee4ece60
mariosangiorgio <mariosangiorgio@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-09-06 23:23:45 +0000

    Added the page to see the list of wedges

--- admin.php
+++ admin.php
@@ -1,4 +1,5 @@
 Here the admin should do some cool stuffs!!!<BR>
 <A HREF=newOrganizer.php>Add a new organizer</A><BR>
+<A HREF=showWedges.php>Veiw the available wedges</A><BR>
 <A HREF=newWedge.php>Add a new wedge</A><BR>
 <A HREF=logout.php>Logout</A><BR>
