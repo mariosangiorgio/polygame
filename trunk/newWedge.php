c9f0a076e62b8a16eb4805661375459e7c370ecb
mariosangiorgio <mariosangiorgio@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-09-06 22:57:42 +0000

    Insertion of a wedge into the database

---
+++ trunk/newWedge.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if( $_SESSION['loggedIn'] == "yes" and
+	$_SESSION['role'] == "administrator"){
+?>
+<FORM METHOD="POST" ACTION="./insertNewWedge.php">
+<TABLE>
+<TR><TD>Wedge title</TD><TD><INPUT TYPE="text" NAME="title"></TD></TR>
+<TR><TD>Introduction</TD><TD><INPUT TYPE="text" NAME="introduction"></TD></TR>
+<TR><TD>History</TD><TD><INPUT TYPE="text" NAME="history"></TD></TR>
+<TR><TD>Present use</TD><TD><INPUT TYPE="text" NAME="presentUse"></TD></TR>
+<TR><TD>National situation</TD><TD><INPUT TYPE="text" NAME="nationalSituation"></TD></TR>
+<TR><TD>Emission reduction</TD><TD><INPUT TYPE="text" NAME="emissionReduction"></TD></TR>
+<TR><TD>Pro</TD><TD><INPUT TYPE="text" NAME="pro"></TD></TR>
+<TR><TD>Cons</TD><TD><INPUT TYPE="text" NAME="cons"></TD></TR>
+<TR><TD>References</TD><TD><INPUT TYPE="text" NAME="references"></TD></TR>
+</TABLE>
+
+<INPUT TYPE="submit" VALUE="Insert">
+</FORM>
+<?php
+}
+else {
+	print "You must log in as an administrator to access this page!";
+}
+?>
