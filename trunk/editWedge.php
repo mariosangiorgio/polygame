6a0fc4c73f8ce9de137bc61f8b9f5c31f10e3f59
mariosangiorgio <mariosangiorgio@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-09-07 00:46:55 +0000

    Fix

--- trunk/editWedge.php
+++ trunk/editWedge.php
@@ -5,17 +5,21 @@ require("./database/databaseLogin.php");
 if( $_SESSION['loggedIn'] == "yes" and
 	$_SESSION['role'] == "administrator"){
 	
-	$wedge		= mysql_real_escape_string($_GET['wedge']);
+	$wedge	= mysql_real_escape_string($_GET['wedge']);
+	$query	= "SELECT * FROM `Wedges` WHERE `Title` = '".$wedge."';";
+	$data	= mysql_query($query,$connection);
+	$row	= mysql_fetch_array($data);
 	
-	$query		= "SELECT * FROM `Wedges` WHERE `Title` = '".$wedge.";";
-	$data		= mysql_query($query,$connection);
-	
-	$row	= mysql_fetch_array($data, MYSQL_BOTH);
-	
-	print	"<TABLE>";
-	for($i=0;$i<9;$i++){
-		print "<TR><TD>".$row[i]."</TD></TR>";
-	}
+	print "<TABLE>";
+	print "<TR><TD>Title</TD><TD>".$row['Title']."</TD></TR>";
+	print "<TR><TD>Introduction</TD><TD>".$row['Introduction']."</TD></TR>";
+	print "<TR><TD>History</TD><TD>".$row['History']."</TD></TR>";
+	print "<TR><TD>Present use</TD><TD>".$row['Present use']."</TD></TR>";
+	print "<TR><TD>National situation</TD><TD>".$row['National situation']."</TD></TR>";
+	print "<TR><TD>Emission reduction</TD><TD>".$row['Emission reduction']."</TD></TR>";
+	print "<TR><TD>Pros</TD><TD>".$row['Pros']."</TD></TR>";
+	print "<TR><TD>Cons</TD><TD>".$row['Cons']."</TD></TR>";
+	print "<TR><TD>references</TD><TD>".$row['References']."</TD></TR>";
 	print	"</TABLE>";
 }	
 else {
