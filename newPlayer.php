87e6d7ab74b32ebcd83fb5a3ef6f0a90478cef1c
gtrive.code <gtrive.code@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-09-07 00:37:57 +0000

    New players funct

---
+++ newPlayer.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+if( $_SESSION['loggedIn'] == "yes" and
+	$_SESSION['role'] == "organizer"){
+?>
+<FORM METHOD="POST" ACTION="./insertNewPlayer.php">
+<TABLE>
+<TR><TD>Name</TD><TD>Password</TD></TR>
+<TR>
+<TD><INPUT TYPE="text" NAME="username"></TD>
+<TD><INPUT TYPE="password" NAME="password"></TD>
+</TR>
+</TABLE>
+
+<INPUT TYPE="submit" VALUE="Insert">
+</FORM>
+<?php
+}
+else {
+	print "You must log in as an organizer to access this page!";
+}
+?>
