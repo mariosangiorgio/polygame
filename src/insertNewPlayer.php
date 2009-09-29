c26cb406779322d08019ecb356c1dc588e874aba
gtrive.code <gtrive.code@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-09-29 22:11:45 +0000

    New organizer options

--- src/insertNewPlayer.php
+++ src/insertNewPlayer.php
@@ -4,6 +4,9 @@ session_start();
 if( $_SESSION['loggedIn'] == "yes" and
 	$_SESSION['role'] == "organizer"){
 	require("./database/databaseLogin.php");
+	//Redirect to the main page
+	header("Location: organize.php");
+	
 	//Sanitizing inputs
 	$username	= mysql_real_escape_string($_POST['username']);
 	$password	= mysql_real_escape_string($_POST['password']);
@@ -13,8 +16,6 @@ if( $_SESSION['loggedIn'] == "yes" and
 	$query		= "INSERT INTO `Users` (`username`,`role`,`password`)
 				   VALUES ('$username', 'player', '$password');";
 	$data		= mysql_query($query,$connection);
-	//Redirect to the main page
-	header("Location: index.php");
 }
 else{
 	print "To perform this operation you must be logged in as an organizer!";
