41630d4bd04d9ac3a41073cb38d12d487c91f9dd
gtrive.code <gtrive.code@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-10-01 17:25:01 +0000

    Choose player for a game

---
+++ src/chooseGamePlayer.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+//Security check
+if( $_SESSION['loggedIn'] == "yes" and
+	$_SESSION['role'] == "organizer"){
+	require("./database/databaseLogin.php");
+	header("Location: organize.php");
+	
+	foreach($_POST['selectedUsers'] as $value)  {
+		$query		= "INSERT INTO `Game Players` (`Game ID`,`Player ID`)
+				   VALUES (
+				   ( SELECT `Game ID` FROM `Game`
+					WHERE `Organizer ID` = '".$_SESSION['username']."' )
+				   , '$value');";
+		$data		= mysql_query($query,$connection);
+	} 
+
+}
+else{
+	print "To perform this operation you must be logged in as an organizer!";
+}
+?>
---
+++ src/insertNewGame.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require("./database/databaseLogin.php");
+
+if( $_SESSION['loggedIn'] == "yes" and
+	$_SESSION['role'] == "organizer"){
+	
+	$length1a	= mysql_real_escape_string($_POST['length1a']);
+	$length1b	= mysql_real_escape_string($_POST['length1b']);
+	$length1c	= mysql_real_escape_string($_POST['length1c']);
+	$length2	= mysql_real_escape_string($_POST['length2']);
+	
+	$query		= "INSERT INTO `Game` (`Organizer ID`,`Starting time`, 
+				  `Length 1a`,`Length 1b`,`Length 1c`,`Length 2`)
+				   VALUES ('".$_SESSION['username']."', NOW(),".$length1a.
+				   ", ".$length1b.", 
+				   ".$length1c.", 
+				   ".$length2." );";
+	$data		= mysql_query($query,$connection);
+
+	if ($data == 1) {
+		header("Location: organize.php");
+		$_SESSION['gamePhase'] = 1;
+	}
+	else {
+		print "Please check the values!\n";
+		print "<BR><A HREF=newGame.php>Back to insert</A><BR>"	;
+	}
+	
+}
+else {
+	print "You must log in as an organizer to access this page!";
+}
+?>
