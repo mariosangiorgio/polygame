d4cfc0f4060f46bd5a6475a9c5b4a88dce8b858e
mariosangiorgio <mariosangiorgio@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-10-01 20:29:50 +0000

    Phase 1 is almost done. Started to work at phase 2. New files added

---
+++ src/createPlan.php
@@ -0,0 +1,63 @@
+<?php
+
+session_start();
+
+?>
+
+<body>
+    <script type="application/javascript">
+    
+    function validate(currentWedge) {
+    	var wedgesUsed = 0;
+    	var i;
+    	var rows = document.getElementById('WedgeTable').rows.length - 1;
+    	var value;
+    	//document.getElementById(currentWedge).value
+    	//	= document.getElementById(currentWedge).value.replace (/\D/, '');
+    	
+    	for($i=0; $i<rows; $i = $i+1){
+    		value = document.getElementById('wedge'+$i).value;
+    		value = parseInt(value);
+    		if( !isNaN(value) ){
+    			wedgesUsed = wedgesUsed + value;
+    		}
+    	}
+    	if(wedgesUsed > 20){
+    		document.getElementById('wedgesQuantity').innerHTML = "<span style='color:red'>Quantity</span>";
+    	}
+    	else{
+    		document.getElementById('wedgesQuantity').innerHTML = "Quantity";
+    	}
+
+    }
+    
+    </script>
+    <noscript>
+      <p>Your browser either does not support JavaScript, or you have JavaScript turned off.</p>
+    </noscript>
+
+<form name="Plan"
+	  method="POST"
+	  action="submitPlan.php">
+<TABLE id="WedgeTable">
+<TR><TD>Wedge</TD><TD id=wedgesQuantity>Quantity</TD></TR>
+
+<?php
+
+//Here I fill all the lines with the wedge data
+for ($i=0; $i<10;$i++){
+	print "<TR><TD>Fakename</TD><TD>";
+	print "<input id=wedge";
+	print $i;
+	print " onkeyup=\"this.value = this.value.replace (/\D/, ''); validate('wedge";
+	print $i;
+	print "');\">";
+	print "</TD></TR>\n";
+}
+?>
+
+</TABLE>
+<input type="submit">
+</form>
+
+</body>
---
+++ src/updatePoster.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+require("./database/databaseLogin.php");
+
+$query 	= 
+	"UPDATE `Posters`
+	 SET
+	 	`Pros`	= '".$_POST['Pros']."',
+	 	`Cons`	= '".$_POST['Cons']."', 
+	 	`Notes`	= '".$_POST['Notes']."'
+	 WHERE `Player`='".$_SESSION['username']."' and 
+	 	   `Game ID`='".$_SESSION['gameID']."' and 
+	 	   `Wedge ID`='".$_SESSION['wedgeID']."';";
+
+$data	= mysql_query($query,$connection);
+$_SESSION['posterSubmitted'] = true;
+
+//Redirect to the main page
+header("Location: showWedgeInformation.php");
+?>
