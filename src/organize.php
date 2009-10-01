26662edcfd6a08867c58b5c11287c982ae3fc1e6
gtrive.code <gtrive.code@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-10-01 17:24:42 +0000

    Choose player for a game

--- src/organize.php
+++ src/organize.php
@@ -1,10 +1,37 @@
+<?php
+session_start();
+if ($_SESSION['gamePhase'] < 2) {
+?>
 <b>Organizer</b><BR>
 Organize a new PolyGame:<BR>
 <A HREF=newPlayer.php>Add new players</A><BR>
-<A HREF=deletePlayers.php>Remove players</A><BR>
-<A HREF=chooseGamePlayers.php>Choose game players</A><BR>
-<A HREF=newVoters.php>Add voters</A><BR><BR>
+<A HREF=deletePlayers.php>Remove players</A><BR><BR>
+
+<?php
+if ($_SESSION['gamePhase'] == 0) {
+?>
+<A HREF=newGame.php>Create a new game</A><BR>
+<?php }
+else if ($_SESSION['gamePhase'] == 1) {
+?>
+<A HREF=deleteGame.php>Abandon game</A><BR>
+<A HREF=showGamePlayers.php>View game players for an existing game</A><BR>
+<A HREF=chooseGamePlayers.php>Choose game players for an existing game</A><BR>
+<A HREF=newVoters.php>Add voters to an existing game</A><BR><BR>
 <A HREF=showWedges.php>View the available wedges</A><BR>
 <A HREF=chooseWedges.php>Choose and edit wedges</A><BR>
 <A HREF=newWedge.php>Add a new wedge</A><BR><BR>
+<?php }
+?>
+
+<?php }
+
+else {
+?>
+
+<A HREF=logout.php>Pause / remove / more time etc...</A><BR>
+
+<?php }
+?>
+
 <A HREF=logout.php>Logout</A><BR>
