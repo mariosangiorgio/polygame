f29a1ad1468636f09398571cf6ba9813aba21148
mariosangiorgio <mariosangiorgio@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-10-28 05:40:25 +0000

    Fix and check solution now works

--- trunk/src/businessLogic/updatePoster.php
+++ trunk/src/businessLogic/updatePoster.php
@@ -7,11 +7,14 @@ require("./databaseLogin.php");
 $query 	= 
 	"UPDATE `Posters`
 	 SET
-	 	`Pros`	= '".$_POST['Pros']."',
-	 	`Cons`	= '".$_POST['Cons']."', 
-	 	`Notes`	= '".$_POST['Notes']."'
+	 	`Pros`	= '".mysql_real_escape_string($_POST['Pros'])."',
+	 	`Cons`	= '".mysql_real_escape_string($_POST['Cons'])."', 
+	 	`Notes`	= '".mysql_real_escape_string($_POST['Notes'])."'
 	 WHERE `Player`='".$_SESSION['username']."' and 
-	 	   `Game ID`='".$_SESSION['gameID']."' and 
+	 	   `Game ID`= (SELECT `Game ID`
+	 	   			   FROM `Game Players`
+	 		   		   WHERE `Player ID` = '".$_SESSION['username']."')
+	 		   		   and 
 	 	   `Wedge ID`='".$_SESSION['wedgeID']."';";
 
 $data	= mysql_query($query,$connection);
---
+++ trunk/src/checkSolutionValue.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+require("./businessLogic/databaseLogin.php");
+
+$query 	= 
+	"SELECT `Solution`, `Error Tolerance`
+	 FROM	`Wedges`, `Wedge Players`
+	 WHERE	`Wedges`.`Wedge ID` = `Wedge Players`.`Wedge ID` and
+	 		`Wedge Players`.`User ID`
+	 			= '".$_SESSION['username']."'";
+$data	= mysql_query($query,$connection);
+$values = mysql_fetch_array($data);
+
+$solution = (float) $_POST['solution'];
+if(abs($solution - $values['Solution'])/$values['Solution']
+		<
+   $values['Error Tolerance']/100){
+   print "Your solution is correct";
+}
+else{
+   print "There is something wrong";
+}
+?>
