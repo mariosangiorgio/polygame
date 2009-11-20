a8ba7828a46d63f2e40bbca7d606a697a6b31016
gtrive.code@gmail.com <gtrive.code@gmail.com@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2009-11-20 09:57:01 +0000

    RC

--- trunk/src/newGame.php
+++ trunk/src/newGame.php
@@ -4,16 +4,29 @@ session_start();
 if( $_SESSION['loggedIn'] == "yes" and
 	$_SESSION['role'] == "organizer"){
 ?>
-<FORM METHOD="POST" ACTION="./businessLogic/insertNewGame.php">
+
+
+<script type="text/javascript">
+<!--
+    function alpha(e) {
+        var k;
+        document.all ? k = e.keyCode : k = e.which;
+return ( (k > 47 && k < 58) || k==8 || k==9 || (k > 36 && k < 41) || k==17);
+}
+// -->
+</script>
+
+
+<FORM METHOD="POST" ACTION="./businessLogic/insertNewGame.php" id="new" action="javascript://">
 <TABLE>
 <TR><TD><b>First phase</b></TD></TR>
-<TR><TD>Delay (minutes) before complete information </TD><TD><INPUT TYPE="text" NAME="length1a" VALUE="20"></TD></TR>
-<TR><TD>Delay (minutes) before submit solution </TD><TD><INPUT TYPE="text" NAME="length1b" VALUE="20"></TD></TR>
-<TR><TD>Delay (minutes) before poster </TD><TD><INPUT TYPE="text" NAME="length1c" VALUE="60"></TD></TR>
+<TR><TD>Delay (minutes) before complete information </TD><TD><INPUT TYPE="text" NAME="length1a" VALUE="20" onkeypress="return alpha(event)"></TD></TR>
+<TR><TD>Delay (minutes) before submit solution </TD><TD><INPUT TYPE="text" NAME="length1b" VALUE="20" onkeypress="return alpha(event)"></TD></TR>
+<TR><TD>Delay (minutes) before poster </TD><TD><INPUT TYPE="text" NAME="length1c" VALUE="60" onkeypress="return alpha(event)"></TD></TR>
 <TR><TD><b>Second phase</b></TD></TR>
-<TR><TD>Length (minutes) of second phase </TD><TD><INPUT TYPE="text" NAME="length2" VALUE="120"></TD></TR>
+<TR><TD>Length (minutes) of second phase </TD><TD><INPUT TYPE="text" NAME="length2" VALUE="120" onkeypress="return alpha(event)"></TD></TR>
 <TR><TD><b>Other values</b></TD></TR>
-<TR><TD>Time (minutes) given for one poster presentation</TD><TD><INPUT TYPE="text" NAME="presentation" VALUE="3"></TD></TR>
+<TR><TD>Time (minutes) given for one poster presentation</TD><TD><INPUT TYPE="text" NAME="presentation" VALUE="3" onkeypress="return alpha(event)"></TD></TR>
 </TABLE>
 
 <INPUT TYPE="submit" VALUE="Insert">
--- trunk/src/rulesAndPlayers.php
+++ trunk/src/rulesAndPlayers.php
@@ -39,7 +39,7 @@ print "<b>Players</b>";
 	?>
 	</table><BR>
 	
-	<BR><BR><b>Phase 2</b><BR>
+	<b>Phase 2</b><BR>
 	<table border=".1.">
 	<?php
 	if( mysql_num_rows($data2)==0 ) print "No players selected";
@@ -49,5 +49,48 @@ print "<b>Players</b>";
 	?>
 	</table><BR>
 <?php
-}	
+}
+	print "<BR><BR><b>Wedges association</b><BR>";
+	
+	// Extract all game wedges
+$query1		= "SELECT `Wedge ID`, `User ID` FROM `Wedge Players`
+              WHERE `Game ID` IN (SELECT `Game ID`
+                                 FROM `Game`
+                                 WHERE `Organizer ID` = '".
+                                 $_SESSION['username']."' ) ;";
+$wedges 	= mysql_query($query1,$connection);
+//print $query1;
+
+if( mysql_num_rows($wedges) == 0 ) {
+	print "No wedges in this game<BR>";
+}
+else
+{
+
+?>
+
+<table border=".1.">
+<TR><TD>Team name</TD><TD>Assigned wedge</TD></TR>
+<?php
+	while( $row	= mysql_fetch_array($wedges))
+	{
+		// Username column
+		print "<TR><TD>".$row['User ID']."</TD>";
+		
+		// Extract wedge name
+		$query2		= "SELECT `Title` FROM `Wedges`
+              WHERE `Wedge ID` = ".$row['Wedge ID']." ;";
+        //print $query2;
+		$qtitle	= mysql_query($query2,$connection);
+		$title	= mysql_fetch_array($qtitle);
+		
+		// Link column
+		print "<TD><A HREF=\"./showPoster.php?wedgeID=".$row['Wedge ID']."\">".$title['Title']."</A></TD>";	
+		
+	}
+?>
+</table><BR>
+
+<?php
+}
 	?>
