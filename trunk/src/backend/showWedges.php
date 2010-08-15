d25183cbca0a3c0b390436264903d497ae52bb5e
francesco.lunetta <francesco.lunetta@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2010-08-15 13:51:14 +0000

    New main page -utilities

---
+++ trunk/src/backend/showWedges.php
@@ -0,0 +1,20 @@
+<?php 
+	session_start();
+	include_once("../inc/db_connect.php");
+	
+	$counter = 0;
+	$lang = $_SESSION['lang'];
+	$query = "SELECT Title, Summary, Image FROM Wedges WHERE Language='$lang' ORDER BY Preferences DESC";
+
+	$result = mysql_query( $query, $connection );
+	while( $row = mysql_fetch_array( $result ))
+	{
+		$wedges['wedge'.$counter] = array( 'title' => $row['Title'],
+											'summary' => $row['Summary'],
+											'image' => $row['Image'] );
+		$counter++;
+	}
+	
+	$result = json_encode( $wedges );
+	echo $result;
+?>
---
+++ trunk/src/inc/common.php
@@ -0,0 +1,43 @@
+<?php
+	session_start();
+	header('Cache-control: private'); // IE 6 FIX
+
+	if( isSet( $_GET['lang']))
+	{
+		$lang = $_GET['lang'];
+		$_SESSION['lang'] = $lang;
+		setcookie( "lang", $lang, time() + ( 3600 * 24 * 30 ));
+	}
+	else if( isSet( $_SESSION['lang']))
+	{
+		$lang = $_SESSION['lang'];
+	}
+	else if( isSet($_COOKIE['lang']))
+	{
+		$lang = $_COOKIE['lang'];
+		$_SESSION['lang'] = $lang;
+	}
+	else
+	{
+		$lang = 'en';
+		$_SESSION['lang'] = $lang;
+	}
+	
+	switch( $lang ) 
+	{
+		case 'en':
+			$lang_file = 'en/en.php';
+		break;
+		case 'de':
+			$lang_file = 'de/de.php';
+		break;
+		case 'it':
+			$lang_file = 'it/it.php';
+		break;
+		default:
+			$lang_file = 'en/en.php';
+
+	}
+
+	include_once 'lang/'.$lang_file;
+?>
