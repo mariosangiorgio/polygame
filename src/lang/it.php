8f7033038c10739add7783dd728e2db1f78b2687
francesco.lunetta <francesco.lunetta@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2010-08-17 19:02:32 +0000

    Right column proposal

---
+++ src/lang/it.php
@@ -0,0 +1,29 @@
+<?php
+	/* 
+	------------------
+	Language: Italian
+	------------------
+	*/
+
+	$TEXT['site_name'] = "Polygame";
+	
+	/* 
+	------------------
+	Main Page: index.php
+	------------------
+	*/
+	
+	$TEXT['main-page_title'] = "Pagina Principale";
+	$TEXT['main-h1_1'] = "(ita) Wondering what Polygame is?";
+	$TEXT['main-h1_2'] = "(ita) Wondering how to reduce emissions?";
+	$TEXT['main-p_1'] = "(ita) Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. <br /><br /> A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.";
+	$TEXT['main-a_1'] = "(ita) more info";
+	$TEXT['main-a_2'] = "(ita) Show more";
+	
+	/* 
+	------------------
+	Wedge Info: wedgeInfo.php
+	------------------
+	*/
+	
+?>
