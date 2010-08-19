e9a613f41c29c5a23efd19b3a52358002ba0cad3
gtrive.code@gmail.com <gtrive.code@gmail.com@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2010-08-19 12:59:11 +0000

    Draft of header now ready (see header_test.php) Fixed an error that was preventing wedge information to be shown

---
+++ trunk/src/header.php
@@ -0,0 +1,16 @@
+<div id="header">
+	<div id="stripe" />
+	<div id="name">
+		Hi, yournamehere!
+	</div>
+	<div id="logo">
+		<img src="images/poly-new.png" height="50px" />
+	</div>
+	<div id="languages-head"> 
+		<a href="index.php?lang=en"><img src="images/en.png" /></a> 
+		<a href="index.php?lang=it"><img src="images/it.png" /></a> 
+	</div>
+	<div id="logout">
+		<a href="logout.php">Logout</a> 
+	</div>
+</div>
---
+++ trunk/src/header_test.php
@@ -0,0 +1,23 @@
+<?php 
+	include_once("inc/db_connect.php");
+	include_once("inc/common.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<title><? echo $TEXT['main-page_title']; ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link href="css/main.css" type="text/css" rel="stylesheet" />
+	<link type="text/css" href="css/ui-lightness/jquery-ui-1.8.4.custom.css" rel="stylesheet" />	
+	<script type="text/JavaScript" src="lib/jquery-1.4.1.min.js"></script>
+	<script type="text/Javascript" src="lib/jquery-ui-1.8.4.custom.min.js"></script> 
+</head>
+<body>
+
+<!-- TEST HEADER -->
+<?php include("header.php"); ?>
+
+
+</body>
+</html>
