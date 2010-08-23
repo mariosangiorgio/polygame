845a455bb8d69164dfd76ef79ae726aa3e00e47c
francesco.lunetta <francesco.lunetta@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2010-08-23 19:17:57 +0000

    Authentication provided via JS

--- trunk/src/backend/showWedges.php
+++ trunk/src/backend/showWedges.php
@@ -1,21 +1,37 @@
 <?php 
 	session_start();
 	include_once("../inc/db_connect.php");
+	include_once("utils.php");
+	
+	$query = "SELECT min(`Wedge ID`) as min, max(`Wedge ID`) as max FROM `Wedges`";
+	$result = mysql_query( $query, $connection );
+	$wedgeIdLimit = mysql_fetch_array( $result );
+	
+	$vector = generateRandomSequence( $wedgeIdLimit['min'], $wedgeIdLimit['max'] );
+	$lang = $_SESSION['lang'];	
+	
+	$query = "SELECT `Wedge ID` as id, Title, Summary, Image FROM Wedges WHERE Language='$lang'";
+	$result = mysql_query( $query, $connection );
 	
 	$counter = 0;
-	$lang = $_SESSION['lang'];
-	$query = "SELECT `Wedge ID` as id, Title, Summary, Image FROM Wedges WHERE Language='$lang' ORDER BY Preferences DESC";
-	
-	$result = mysql_query( $query, $connection );
 	while( $row = mysql_fetch_array( $result ))
 	{
-		$wedges['wedge'.$counter] = array(  'id' 		=> $row['id'],
-											'title' 	=> $row['Title'],
-											'summary' 	=> $row['Summary'],
-											'image' 	=> $row['Image'] );
+		$wedges[$counter] = array(  'id' 		=> $row['id'],
+									'title' 	=> $row['Title'],
+									'summary' 	=> $row['Summary'],
+									'image' 	=> $row['Image'] );
 		$counter++;
 	}
 	
-	$result = json_encode( $wedges );
+	$index = 0;
+	$result = array();
+	while( $counter > 0 )
+	{
+		$result['wedge'.$index] = $wedges[$vector[$index]-1];
+		$index++;
+		$counter--;
+	}
+	
+	$result = json_encode( $result );
 	echo $result;
 ?>
---
+++ trunk/src/wedgeInfo.php
@@ -0,0 +1,112 @@
+<?php
+	session_start();
+	include_once("./inc/db_connect.php");
+	include_once("./inc/common.php");
+	include_once("./backend/utils.php");
+	
+	$wedgeInfo = null;
+	$wedgeId = null;
+	
+	if( isSet( $_GET['id']))
+	{
+		$wedgeId = $_GET['id'];
+		$lang = $_SESSION['lang'];
+		$query = "SELECT Title, Introduction, History, Image FROM Wedges WHERE Language='$lang' AND `Wedge ID`=$wedgeId";
+
+		$result = mysql_query( $query, $connection );
+		$wedgeInfo = mysql_fetch_array( $result );
+	}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<title><? echo $wedgeInfo['Title']; ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link href="css/main.css" type="text/css" rel="stylesheet" />
+	<link type="text/css" href="css/ui-lightness/jquery-ui-1.8.4.custom.css" rel="stylesheet" />	
+	<script type="text/JavaScript" src="lib/jquery-1.4.1.min.js"></script>
+	<script type="text/Javascript" src="lib/jquery-ui-1.8.4.custom.min.js"></script>
+	<script type="text/Javascript"> 
+	
+	( function($) {
+		$(document).ready( function() 
+		{
+			var accordionOption = { collapsible: true, 
+									active : false, 
+									autoHeight: false, 
+									animated: 'bounceslide' };
+			
+			$("#wedgeList").accordion( accordionOption );
+		});
+	})(jQuery);
+	</script> 
+</head>
+<body>
+	<div id="header">
+		<div id="stripe" />
+		<div id="logo">
+			<img src="images/poly-new.png" height="75px" />
+		</div>
+		<div id="languages"> 
+			<a href="index.php?lang=en"><img src="images/en.png" /></a> 
+			<a href="index.php?lang=it"><img src="images/it.png" /></a> 
+		</div>
+	</div>
+	<div id="wrapper">
+		<div id="columnLeft">
+			<div id="top">
+				<h1><? echo $wedgeInfo['Title']; ?></h1>
+				<p><? echo $wedgeInfo['Introduction']; ?></p>
+			</div>
+			<div id="image">
+				<img src="<? echo $wedgeInfo['Image']; ?>" />
+			</div>
+			<div id="bottom">
+				<p><? echo $wedgeInfo['History']; ?></p>
+			</div>
+		</div>
+		<div id="columnRight">
+			<div id="wedgeList" class="accordion">
+<? 
+	$query = "SELECT `Wedge ID` as id, Title, Summary, Image ". 
+			 "FROM Wedges ".
+			 "WHERE Language='$lang' AND ( `Wedge ID` <> $wedgeId )";
+	$data = mysql_query( $query, $connection );
+	
+	$counter = 0;
+	while( $wedge = mysql_fetch_array( $data )) 
+	{
+		$wedges[$counter] = array(  'Id' 		=> $wedge['id'],
+									'Title' 	=> $wedge['Title'],
+									'Summary' 	=> $wedge['Summary'],
+									'Image' 	=> $wedge['Image'] );
+		$counter++;
+	}
+	
+	$vector = generateRandomSequence( 0, $counter );
+	$index = 0;
+	while( $counter > 0 )
+	{
+		$wedge = $wedges[$vector[$index]];
+?>
+				<h3><a><? echo $wedges[$vector[$index]]['Title']; ?></a></h3>
+				<div>
+					<img src="<? echo $wedge['Image']; ?>" width="66px" height="84px" />
+					<p class="accordionText">
+						<? echo $wedge['Summary']; ?>
+					</p>
+					<p class="accordionLink">
+						<a href="wedgeInfo.php?id=<? echo $wedge['Id']; ?>"><? echo $TEXT['main-a_1']; ?></a>
+					</p>
+				</div>
+<?		
+		$counter--;
+		$index++;
+	}
+?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
