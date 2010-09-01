436190ad5d54cbb6b88f474433a09dcb30f17816
francesco.lunetta <francesco.lunetta@08fd447c-7e85-11de-b420-6b84dc2c4c10>
2010-09-01 13:49:45 +0000

    Authentication logic

---
+++ trunk/src/backend/authentication.php
@@ -0,0 +1,43 @@
+<?php
+	session_start();
+	include_once("../inc/db_connect.php");
+
+	//Sanitizing inputs
+	$username = mysql_real_escape_string( $_POST['username']);
+	$password = mysql_real_escape_string( $_POST['password']);
+
+	$query = "SELECT `username`,`role`,`password` FROM `Users` WHERE `username`='$username'";
+	$result = mysql_query( $query, $connection );
+	
+	if( mysql_num_rows( $result ))	
+	{
+		// User exists, check if password is correct
+		$user = mysql_fetch_array( $result );
+		if( $password == $user['password'] )
+		{
+			// Password is correct
+			$_SESSION['loggedIn']	= "yes";
+			$_SESSION['username']	= $username;
+			$_SESSION['role']		= $user['role'];
+		
+			if( $_POST['usingAjax'])
+				$response = array(  'code' => "200",
+									'message' => 'OK' );
+		}
+		else
+		{
+			// Password isn't correct
+			if( $_POST['usingAjax'])
+				$response = array(  'code' => "401",
+									'message' => 'Invalid Password' );
+		}
+	}
+	else{
+		// User not exists
+		$response = array(  'code' => "402",
+							'message' => 'User not exists' );
+	}
+	
+	$response = json_encode( $response );
+	echo $response;
+?>
